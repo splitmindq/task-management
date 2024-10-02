b359f41409dcda7bb928ab25209e549e789179c2
splitmindq <mzitraksqq@gmail.com>
2024-10-02 15:50:14 +0300

    refactoring

---
+++ src/headers/User.h
@@ -0,0 +1,20 @@
+//
+// Created by mzitr on 16.09.2024.
+//
+
+#ifndef LABS_USER_H
+#define LABS_USER_H
+
+#include <memory>
+#include <string>
+
+class User {
+public:
+    int id;
+    std::unique_ptr<std::string> email;
+
+    User(int id, const std::string& email);
+    ~User() = default;
+};
+
+#endif // LABS_USER_H
